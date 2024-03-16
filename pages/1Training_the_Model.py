fcb7106006b26c024fab0dbb48301bb61e3df541
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-16 23:20:02 +0000

    Save

--- pages/1Training_the_Model.py
+++ pages/1Training_the_Model.py
@@ -153,7 +153,7 @@ def app():
             time.sleep(0.01)
         # Progress bar reaches 100% after the loop completes
         st.success("Model training completed!") 
-        st.write("Use the sidebar to open the Performance page.")
+        st.write("The model is now trained to tell dogs and cats apart. Use the sidebar to open the Performance Testing page.")
 
 # Define a function to plot images
 def plot_images(images, labels):
