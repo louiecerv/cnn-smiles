d753c5a71ddb66fbf926e8170f92efe8974442ac
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-17 16:00:38 +0800

    Save

--- pages/1Training_the_Model.py
+++ pages/1Training_the_Model.py
@@ -96,7 +96,7 @@ def app():
     
    # Define CNN parameters    
     st.sidebar.subheader('Set the CNN Parameters')
-    options = ["relu", "leakyrelu", "tanh", "elu", "selu"]
+    options = ["relu", "tanh", "elu", "selu"]
     h_activation = st.sidebar.selectbox('Activation function for the hidden layer:', options)
 
     options = ["sigmoid", "softmax"]
